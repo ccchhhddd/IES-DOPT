622d2041a1831cebd899276ef8333085a4a572b9
caochuanwu <1912697605@qq.com>
2024-01-24 20:26:40 +0800

    小补丁

--- backend/src/head.jl
+++ backend/src/head.jl
@@ -5,13 +5,11 @@
   using CoolProp
   using OrderedCollections
 	using Dierckx
+	using Plots
 
 #import CoolProp.PropsSI
   PropsSI(out::AbstractString, name1::AbstractString, value1::Real, name2::AbstractString, value2::Real, fluid::AbstractString) = CoolProp.PropsSI(out, name1, value1, name2, value2, fluid)
   @register_symbolic PropsSI(out::AbstractString, name1::AbstractString, value1::Real, name2::AbstractString, value2::Real, fluid::AbstractString)
-
-@info "定义工质..."
-# fluid = "Water"
 
 # 定义独立时间变量
 @info "定义变量..."
